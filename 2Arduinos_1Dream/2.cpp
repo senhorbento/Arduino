40b4d95771e4554537015df03bc5ba72361cea5a
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2021-10-07 08:10:19 -0300

    Add files via upload

---
+++ 2Arduinos_1Dream/2.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+#define PinR  7
+#define PinG  6 
+#define PinB  5         
+
+
+void estado(int v){
+  if(v == 0){
+      digitalWrite(PinR, 0);
+      digitalWrite(PinG, 0);
+      digitalWrite(PinB, 0);
+      Serial.write('0');
+  }
+  else{
+      digitalWrite(PinR, 1);
+      digitalWrite(PinG, 1);
+      digitalWrite(PinB, 1);
+      Serial.write('1');
+  }
+}
+
+void setup() { 
+  Serial.begin(9600);
+  pinMode(PinR, OUTPUT);
+  pinMode(PinG, OUTPUT);
+  pinMode(PinB, OUTPUT);
+  estado(0);  
+} 
+  
+void loop() {
+  char recebido;
+  
+  if(Serial.available()){
+    recebido = Serial.read();
+        
+    if(recebido == '1')
+      estado(1);
+
+    else if (recebido == '0')
+      estado(0);
+  }
+
+   delay(1000);
+}
