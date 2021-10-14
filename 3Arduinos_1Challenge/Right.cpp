da401ba1c83d9bdb5f50cd35be0388b62b87bf8c
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2021-10-13 23:41:11 -0300

    Update Right.cpp

--- 3Arduinos_1Challenge/Right.cpp
+++ 3Arduinos_1Challenge/Right.cpp
@@ -4,17 +4,25 @@
 
 void estado(int v){
   if(v == 0){
-      digitalWrite(PinR, 0);
-      digitalWrite(PinG, 0);
-      digitalWrite(PinB, 0);
+      apagar();
       Serial.write('0');
   }
   else{
-      digitalWrite(PinR, 1);
-      digitalWrite(PinG, 1);
-      digitalWrite(PinB, 1);
+      acender();
       Serial.write('1');
   }
+}
+
+void apagar(){
+  digitalWrite(PinR, 0);
+  digitalWrite(PinG, 0);
+  digitalWrite(PinB, 0);
+}
+
+void acender(){
+  digitalWrite(PinR, 1);
+  digitalWrite(PinG, 1);
+  digitalWrite(PinB, 1);
 }
 
 void setup() { 
@@ -22,6 +30,7 @@ void setup() {
   pinMode(PinR, OUTPUT);
   pinMode(PinG, OUTPUT);
   pinMode(PinB, OUTPUT);
+  estado(1);
 } 
   
 void loop() {
